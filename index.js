2bc357ca2462f58c07824f0f32df066b991dd4b9
jVillaveces <secevalliv@gmail.com>
2015-01-16 16:29:10 +0100

    Two slides more

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ var color = d3.scale.category20();
 var height = document.body.clientHeight;
 var width = document.body.clientWidth;
 
-var svg = d3.select('#db')
-    .attr('width', width)
-    .attr('height', height);
-
-var force = d3.layout.force()
-    .size([width, height])
-    .charge(-200)
-    .on('tick', function(e) {
-        svg.selectAll("g").attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-    });
-
-var size = d3.scale.pow()
-    .range([400,1000]);
+var data = {};
 
 d3.xml('https://cors-anywhere.herokuapp.com/http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=STATUS&format=xml', function(xml) {
-    var servers = [], min = 0, max = 0;
+    var intcount = 0, servers = [];
     d3.select(xml).selectAll('service').each(function() {
         var el = d3.select(this);
         
@@ -29,43 +17,15 @@ d3.xml('https://cors-anywhere.herokuapp.com/http://www.ebi.ac.uk/Tools/webservic
             active : el.select('active').text() === 'true' ? true : false
         };
         servers.push(server);
+        intcount += server.count;
     });
-    
-    min = d3.min(servers, function(d){
-        return d.count;
-    });
-    
-    max = d3.max(servers, function(d){
-        return d.count;
-    });
-    
-    size.domain([min, max]);
-    
-    force.nodes(servers).start();
-    
-    var node = svg.selectAll(".node")
-        .data(servers)   
-        .enter().append("g")
-          .attr("class", "node")
-          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-    node.append("title")
-        .text(function(d) { return d.name; });
-
-    node.append("path")
-        .attr("d", d3.svg.symbol()
-            .size(function(d) { return size(d.count); })
-            .type(function(d) { return 'circle'; }))
-        .style("fill", function(d) { return d.active ? 'rgb(44, 160, 44)' : 'rgb(214, 39, 40)'; })
-        .style("stroke", "white")
-        .style("stroke-width", "1px");
-
-    node.append("text")
-        .attr('y', 25)
-        .style("font-size","18px")
-        .style("fill", "#fff")
-        .style("text-anchor", "middle")
-        .style("font-family", "'Source Sans Pro', Helvetica, sans-serif")
-        .text(function(d) { return d.name; });
-    
+    data.psi = {servers: servers, count : intcount};
 });
+
+Reveal.addEventListener( 'dbs', function() {
+    require('./js/dbs.js')(data.psi, width, height, d3);
+}, false );
+
+Reveal.addEventListener( 'viz', function() {
+    require('./js/viz.js')(['A','B','C','D','E','F','G','H','I','J','K','L','M','N'], width, height, d3);
+}, false );
